c08a6283c65c90f41ce8b1defe26183c89b0b339
Ericson Weah <ericson.weah@utah.edu>
2021-06-04 17:07:23 -0600

    updated api keys

--- config/env/.env/index.js
+++ config/env/.env/index.js
@@ -3,23 +3,21 @@ module.exports = {
 
     // Stripe API Key
     STRIPE: {
-        _API_KEY: 'pk_test_PwefGqRnoIj7yJpcRkurmxGc008cU3VtGR'
+        _API_KEY: 'sk_test_kLLlxWePPsoJfJWmzvIDEdp400mpRjMxUi'
     },
 
     // Mailgun  API Key
     MAILGUN: {
-        _API_KEY: 'f9f52febcd04196673ec1217a539627a-1d8af1f4-255aa97c',
+        _API_KEY: 'key-17cfda8340f7903445011000f41e2e82',
         _DOMAIN: 'mail.devoutprogrammer.io'
-        // _DOMAIN: 'sandboxb907f3fa427748f6909b2315406714f9.mailgun.org'
+
     },
 
     // Twilio API Key
     TWILIO: {
         _ACCOUNT_SID: 'AC931fb10e9db19f607c83228b61218d83',
-        _AUTH_TOKEN: '04f4034d8861a18f0d7562e0d277cdab'
+        _AUTH_TOKEN: 'd71021cf5c469f2c9afe0f8d8136e283'
 
-        // _ACCOUNT_SID: 'ACd865c44151ee3c11c82aa88fd645fab4',
-        // _AUTH_TOKEN: '081c9aa0552a7757ba690b3273bd81af'
     },
     PASSWORD: {
         _HASH: {
