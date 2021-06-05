f46605326910713402cdd265fced5f8571517a29
Ericson Weah <ericson.weah@utah.edu>
2021-06-04 19:05:45 -0600

    updated api keys

--- config/env/.env/index.js
+++ config/env/.env/index.js
@@ -10,14 +10,12 @@ module.exports = {
     MAILGUN: {
         _API_KEY: 'key-17cfda8340f7903445011000f41e2e82',
         _DOMAIN: 'mail.devoutprogrammer.io'
-
     },
 
     // Twilio API Key
     TWILIO: {
         _ACCOUNT_SID: 'AC931fb10e9db19f607c83228b61218d83',
         _AUTH_TOKEN: 'd71021cf5c469f2c9afe0f8d8136e283'
-
     },
     PASSWORD: {
         _HASH: {
