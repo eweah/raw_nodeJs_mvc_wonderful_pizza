c2e2da5c0af355865468e81f0cf3d2dd79600ace
Ericson Weah <ericson.weah@utah.edu>
2022-02-06 09:03:49 -0700

    back

--- config/env/.env/index.js
+++ config/env/.env/index.js
@@ -9,17 +9,16 @@ module.exports = {
     // Mailgun  API Key
     MAILGUN: {
         _API_KEY: 'key-17cfda8340f7903445011000f41e2e82',
-        _DOMAIN: 'mail.devoutprogrammer.io'
-        // _DOMAIN: 'sandboxb907f3fa427748f6909b2315406714f9.mailgun.org'
+        _DOMAIN: 'mail.ericsonweah.com'
+            // _DOMAIN: 'sandboxb907f3fa427748f6909b2315406714f9.mailgun.org'
     },
 
     // Twilio API Key
     TWILIO: {
         _ACCOUNT_SID: 'AC931fb10e9db19f607c83228b61218d83',
         _AUTH_TOKEN: 'd71021cf5c469f2c9afe0f8d8136e283'
-
-        // _ACCOUNT_SID: 'ACd865c44151ee3c11c82aa88fd645fab4',
-        // _AUTH_TOKEN: '081c9aa0552a7757ba690b3273bd81af'
+            // _ACCOUNT_SID: 'ACd865c44151ee3c11c82aa88fd645fab4',
+            // _AUTH_TOKEN: '081c9aa0552a7757ba690b3273bd81af'
     },
     PASSWORD: {
         _HASH: {
@@ -28,4 +27,3 @@ module.exports = {
         }
     }
 }
-
